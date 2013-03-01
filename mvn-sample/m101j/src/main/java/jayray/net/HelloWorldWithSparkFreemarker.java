d09a37ca9dfdb36357d13c1e055d2873e924d481
Jason Ray <jason.ray@agilex.com>
2013-02-28 23:59:24 -0500

    renamed hello world files
    Signed-off-by: Jason Ray <jason.ray@agilex.com>

--- mvn-sample/m101j/src/main/java/jayray/net/HelloWorldWithSparkFreemarker.java
+++ mvn-sample/m101j/src/main/java/jayray/net/HelloWorldWithSparkFreemarker.java
@@ -21,7 +21,7 @@ import java.util.Map;
  * Time: 11:00 PM
  * To change this template use File | Settings | File Templates.
  */
-public class HelloWorldWithSparkFreemarkMongo {
+public class HelloWorldWithSparkFreemarker {
     public static void main(String[] args) {
 
         Spark.get(new Route("/") {
--- mvn-sample/m101j/src/main/java/jayray/net/HelloWorldWithSparkFreemarkerMongo.java
+++ mvn-sample/m101j/src/main/java/jayray/net/HelloWorldWithSparkFreemarkerMongo.java
@@ -4,6 +4,7 @@ import com.mongodb.*;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+import org.bson.types.ObjectId;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -23,13 +24,23 @@ import java.util.Map;
  * Time: 11:00 PM
  * To change this template use File | Settings | File Templates.
  */
-public class HelloWorldWithSparkAndFreemarker {
+public class HelloWorldWithSparkFreemarkerMongo {
     public static void main(String[] args) {
 
         Spark.get(new Route("/") {
             @Override
             public Object handle(final Request request, final Response response) {
-                return getView(request.queryParams("id"));
+                BasicDBObject data = null;
+                try {
+                    data = getData(request.queryParams("id"));
+                } catch (UnknownHostException e) {
+                    halt(500);
+                }
+
+                if (data == null)
+                    halt(404, "not found");
+
+                return getView(data);
             }
         }
 
@@ -37,8 +48,7 @@ public class HelloWorldWithSparkAndFreemarker {
 
     }
 
-    private static Writer getView(String id) {
-        if (id == null) id = "";
+    private static Writer getView(Map data) {
 
         Configuration configuration = new Configuration();
         configuration.setClassForTemplateLoading(HelloWorldFreemarker.class, "/");
@@ -46,7 +56,7 @@ public class HelloWorldWithSparkAndFreemarker {
         StringWriter writer = new StringWriter();
         try {
             Template helloTemplate = configuration.getTemplate("hello.ftl");
-            helloTemplate.process(getData(), writer);
+            helloTemplate.process(data, writer);
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (TemplateException e) {
@@ -55,13 +65,22 @@ public class HelloWorldWithSparkAndFreemarker {
         return writer;
     }
 
-    private static DBObject getData() throws UnknownHostException {
+    private static BasicDBObject getData(String id) throws UnknownHostException {
+
         MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
 
         DB database = client.getDB("test");
         DBCollection collection = database.getCollection("things");
 
-        DBObject data = collection.findOne();
+
+        BasicDBObject data;
+        try {
+            BasicDBObject prototype = new BasicDBObject("_id", new ObjectId(id));
+            data = (BasicDBObject) collection.findOne(prototype);
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+            throw new RuntimeException(ex);
+        }
         return data;
     }
 
