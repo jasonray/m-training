86195ffebd64b9a873a17c1cd260480a4d51b2c9
Jason Ray <jason.ray@agilex.com>
2013-02-28 22:20:39 -0500

    hello world with spark

---
+++ mvn-sample/m101j/src/main/java/jayray/net/HelloWorldSpark.java
@@ -0,0 +1,24 @@
+package jayray.net;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rayj
+ * Date: 2/28/13
+ * Time: 10:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorldSpark {
+    public static void main(String[] args) {
+        Spark.get(new Route("/") {
+            @Override
+            public Object handle(final Request request, final Response response) {
+                return "hello world from spark";
+            }
+        });
+    }
+}
