10873a9ed8152b5f27b6c77ba19a8e220ce736ad
Jason Ray <jasonray@jayray.net>
2013-03-09 11:01:54 -0500

    query by prototype

--- mvn-sample-2/mvn-sample-2/src/main/java/jayray/net/Week2InsertFetchCursor.java
+++ mvn-sample-2/mvn-sample-2/src/main/java/jayray/net/Week2InsertFetchCursor.java
@@ -3,6 +3,7 @@ package jayray.net;
 import com.mongodb.*;
 
 import java.net.UnknownHostException;
+import java.util.Random;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,19 +20,37 @@ public class Week2InsertFetchCursor {
             DB db = client.getDB("test");
             DBCollection myCollection = db.getCollection("w2collection");
 
+            System.out.println("start count: " + myCollection.count());
+
             myCollection.drop();
 
+            System.out.println("post-drop count: " + myCollection.count());
+
             for (int i = 0; i < 10; i++) {
-                DBObject doc = new BasicDBObject().append("data", "something").append("num", i);
+                DBObject doc = new BasicDBObject().append("data", "something").append("num", i).append("x", new Random().nextInt(2)).append("y", new Random().nextInt(100));
                 System.out.println(doc);
                 myCollection.insert(doc);
             }
+
+            System.out.println("post-insert count: " + myCollection.count());
 
             DBCursor cursor = myCollection.find();
             try {
                 while (cursor.hasNext()) {
                     DBObject record = cursor.next();
                     System.out.println("found:" + record);
+                }
+            } finally {
+                cursor.close();
+            }
+
+
+            DBObject prototype = new BasicDBObject("x", 0);
+            cursor = myCollection.find(prototype);
+            try {
+                while (cursor.hasNext()) {
+                    DBObject record = cursor.next();
+                    System.out.println("found this with 0:" + record);
                 }
             } finally {
                 cursor.close();
