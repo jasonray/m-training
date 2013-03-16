82cfa8708bd39018e6bdb70128cb54cd1b43b3a3
Jason Ray <jasonray@jayray.net>
2013-03-16 14:32:52 -0400

    finished 3.1

--- week3/hw3.1/students/src/main/java/jayray/net/hw3.java
+++ week3/hw3.1/students/src/main/java/jayray/net/hw3.java
@@ -1,9 +1,12 @@
 package jayray.net;
 
 import com.mongodb.*;
+import com.mongodb.util.JSON;
 import org.bson.types.ObjectId;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,36 +18,81 @@ import java.net.UnknownHostException;
 public class hw3 {
     public static void main(String[] args) throws UnknownHostException {
         MongoClient client = new MongoClient();
-        DB db = client.getDB("students");
-        DBCollection grades = db.getCollection("grades");
+        DB db = client.getDB("school");
 
-        System.out.println("number of scores: " + grades.count());
+        DBCollection studentsCollection = db.getCollection("students");
+        System.out.println("number of students: " + studentsCollection.count());
 
-        DBObject query = QueryBuilder.start("type").is("homework").get();
-        DBCursor cursor = grades.find(query);
+        DBCursor studentsCursor = studentsCollection.find();
+        while (studentsCursor.hasNext()) {
+            DBObject studentRecord = studentsCursor.next();
+            ArrayList<DBObject> scores = (ArrayList<DBObject>) studentRecord.get("scores");
+            Object studentId =  studentRecord.get("_id");
+            System.out.println("student " + studentId + "; " + scores.size());
 
-        DBObject sort = new BasicDBObject("student_id", 1).append("score", 1);
-        cursor.sort(sort);
+            DBObject lowestHomeworkScoreRecord = null;
 
-        int currentStudent = -1;
-        while (cursor.hasNext()) {
-            DBObject record = cursor.next();
+            for (DBObject score : scores) {
+                String scoreType = (String) score.get("type");
+                Double scoreValue = (Double) score.get("score");
+                System.out.println(String.format("score: type=%s; score:%s", scoreType, scoreValue));
 
-
-            int recordStudentId = Integer.parseInt(record.get("student_id").toString());
-
-            if (currentStudent != recordStudentId) {
-                System.out.println("switching from " + currentStudent + " to " + recordStudentId + ", dropping this record");
-                grades.remove(record);
-                currentStudent = recordStudentId;
-                System.out.println("drop record=" + record);
-            } else {
-                System.out.println("keep record=" + record);
+                if (scoreType.contentEquals("homework")) {
+                    if (lowestHomeworkScoreRecord == null) {
+                        lowestHomeworkScoreRecord = score;
+                    } else {
+                        Double lowestScoreValue = (Double) lowestHomeworkScoreRecord.get("score");
+                        if (scoreValue < lowestScoreValue) {
+                            lowestHomeworkScoreRecord = score;
+                        }
+                    }
+                }
             }
+
+            if (lowestHomeworkScoreRecord != null) {
+                System.out.println("need to remove this score: " + lowestHomeworkScoreRecord);
+
+                scores.remove(lowestHomeworkScoreRecord);
+
+                BasicDBObject thisStudentFilter = new BasicDBObject("_id", studentId);
+                BasicDBObject updateCommand = new BasicDBObject("scores", scores);
+                studentsCollection.update(thisStudentFilter, updateCommand);
+            }
+
 
         }
 
-        System.out.println("number of scores: " + grades.count());
+//        DBObject query = QueryBuilder.start("_id").is("100").get();
+//        System.out.println("query: "  + JSON.serialize(query)  );
+//        DBObject student = studentsCollection.findOne(query);
+//
+//        Object rawScores = student.get("scores");
+//        System.out.println("raw scores: " + rawScores);
+//        List<String> scores = (List<String>) rawScores;
+//
+//        for (String score : scores)  {
+//
+//        }
+
+//        int currentStudent = -1;
+//        while (cursor.hasNext()) {
+//            DBObject record = cursor.next();
+//
+//
+//            int recordStudentId = Integer.parseInt(record.get("student_id").toString());
+//
+//            if (currentStudent != recordStudentId) {
+//                System.out.println("switching from " + currentStudent + " to " + recordStudentId + ", dropping this record");
+//                grades.remove(record);
+//                currentStudent = recordStudentId;
+//                System.out.println("drop record=" + record);
+//            } else {
+//                System.out.println("keep record=" + record);
+//            }
+//
+//        }
+
+        System.out.println("number of students: " + studentsCollection.count());
 
     }
 }
